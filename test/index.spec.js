456ef041d015fdf82c7a4012d42d08fdb2fa7af0
Lukas Siemon <lukas.siemon@gmail.com>
2019-03-09 17:41:11 -0800

    amend: now managing dependabot config

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,5 +2,8 @@ const expect = require('chai').expect;
 const robo = require('../src/index');
 
 it('Executing Configuration', () => {
-  expect(robo(['circleci/npm-release'])).to.deep.equal([]);
+  expect(robo([
+    'circleci/npm-release',
+    'dependabot/nodejs-instant'
+  ])).to.deep.equal([]);
 });
