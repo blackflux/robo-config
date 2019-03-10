cbcb3c29ea7ab56c27f120d3da96c21361c18e97
Lukas Siemon <lukas.siemon@gmail.com>
2019-03-10 16:14:21 -0700

    amend: now validating loaded configurations

--- src/util/config.js
+++ src/util/config.js
@@ -2,8 +2,24 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const deepmerge = require('deepmerge');
+const Joi = require('joi');
 const sls = require('smart-fs');
 const { populateVars } = require('./vars');
+
+
+const configSchema = Joi.object().keys({
+  target: Joi.string(),
+  variables: Joi.object(),
+  modules: Joi.array().items(Joi.object().keys({
+    name: Joi.string().required(),
+    variables: Joi.object().required()
+  }).unknown(false).required()),
+  configs: Joi.array().items(Joi.string())
+})
+  .with('target', 'variables')
+  .with('target', 'modules')
+  .unknown(false)
+  .required();
 
 
 const loadModule = (moduleDir, moduleName, config, moduleVars) => {
@@ -28,24 +44,24 @@ module.exports.loadConfig = (configName, variables) => {
   assert(variables instanceof Object && !Array.isArray(variables));
 
   // load config file
-  const configFilePath = path.join(__dirname, '..', 'template', `${configName}.json`);
+  const configFilePath = path.join(__dirname, '..', 'configs', `${configName}.json`);
   if (!fs.existsSync(configFilePath)) {
     return null;
   }
   const config = sls.smartRead(configFilePath);
 
+  assert(Joi.validate(config, configSchema).error === null, `Invalid Config Detected: ${configName}`);
+
   if (typeof config.target === 'string') {
     Object.assign(config, { variables: populateVars(config.variables, variables, true) });
 
     // load and merge config modules into config
-    const moduleDir = path.join(__dirname, '..', 'template', configName.split('/')[0], 'modules');
+    const moduleDir = path.join(__dirname, '..', 'configs', configName.split('/')[0], 'modules');
     config.toWrite = deepmerge.all(config.modules
       .map(m => [m.name, m.variables])
       .map(([moduleName, moduleVars]) => [moduleName, populateVars(moduleVars, config.variables, true)])
       .map(([moduleName, moduleVars]) => loadModule(moduleDir, moduleName, config, moduleVars)));
   }
-
-  // todo: validate config here using joi
 
   return config;
 };
