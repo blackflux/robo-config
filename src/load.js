635efaa6a21d27a4708a75716605313150f12b81
Lukas Siemon <github@blackflux.com>
2020-03-26 18:48:39 -0700

    feat: tests now checking for rouge test targets
    https://github.com/blackflux/robo-config/issues/1041

--- src/load.js
+++ src/load.js
@@ -4,6 +4,7 @@ const fs = require('smart-fs');
 const Joi = require('joi-strict');
 const { syncDocs, generateDocs } = require('./plugin/docs');
 const { applyTasksRec, listPublicTasks, extractMeta } = require('./plugin/task');
+const { populateVars } = require('./plugin/vars');
 
 module.exports = (pl) => {
   Joi.assert(pl, Joi.object().keys({
@@ -40,13 +41,21 @@ module.exports = (pl) => {
     generateDocs: (taskNames, exclude) => genDocs(taskNames, exclude),
     apply: applyTasks,
     test: (testRoot, variables = {}) => {
+      const knownTargets = fs
+        .walkDir(testRoot)
+        .reduce((p, c) => Object.assign(p, { [c]: false }), {});
       const taskNames = listPublicTasks(pl.taskDir);
       const result = {};
       const knownVars = [];
       taskNames.forEach((taskName) => {
         const taskRoot = path.join(testRoot, taskName);
-        const taskVars = extractMeta(pl.taskDir, [taskName]).variables
-          .reduce((p, c) => Object.assign(p, { [c]: variables[c] || c }), {});
+        const meta = extractMeta(pl.taskDir, [taskName]);
+        const taskVars = meta.variables.reduce((p, c) => Object.assign(p, { [c]: variables[c] || c }), {});
+        meta.target.forEach((t) => {
+          knownTargets[path.join(taskName, populateVars({ t }, taskVars, true).t)] = true;
+        });
+        knownTargets[path.join(taskName, 'CONFDOCS.md')] = true;
+        knownTargets[path.join(taskName, '.roboconfig.lock')] = true;
         knownVars.push(...Object.keys(taskVars));
         const taskResult = applyTasks(taskRoot, [taskName], taskVars, []);
         if (fs.smartWrite(path.join(taskRoot, 'CONFDOCS.md'), genDocs([taskName], []))) {
@@ -58,6 +67,10 @@ module.exports = (pl) => {
         Object.keys(variables).filter((v) => !knownVars.includes(v)).length === 0,
         `Unexpected variable provided: ${Object.keys(variables).filter((v) => !knownVars.includes(v)).join(', ')}`
       );
+      const rougeTargets = Object
+        .entries(knownTargets)
+        .filter(([_, v]) => v !== true);
+      assert(rougeTargets.length === 0, `Rouge test targets detected: ${rougeTargets}`);
       return result;
     }
   });
